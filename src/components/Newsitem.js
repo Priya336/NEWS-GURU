71a38d02e6446c6653433e6a70bfb24394a2eacc
priya goswami <124695610+Priya336@users.noreply.github.com>
2023-07-31 01:42:11 -0700

    Update Newsitem.js
    updating the component

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,23 +4,22 @@ export class Newsitem extends Component {
   render() {
     let { title, description, url, newsurl,author,date ,source} = this.props;
     return (
-      <div>
-        
-        <div className="card" style={{ width: "18rem" }}>
-        
+      
+        <div className="card " style={{backgroundColor:'#6bad1d', width:400 }}>       
           <img src={url} className="card-img-top" alt="..." />
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+
+          <span  className="position-absolute top-0 start-0translate-middle badge rounded-pill bg-danger ">
    {source}
-    <span class="visually-hidden">unread messages</span>
+    <span className="visually-hidden">unread messages</span>
   </span>
           <div className="card-body">
             <h5 className="card-title"> {title}....</h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small class="text-danger">BY {!author?"UNKNOWN":author} on {new Date(date).toGMTString()} </small></p>
+            <p className="card-text"><h5 className="text-danger">BY {!author?"UNKNOWN":author} on {new Date(date).toGMTString()} </h5></p>
             <a href={newsurl} target="_blank" className="btn  btn-sn btn-dark">Read More</a>
           </div>
         </div>
-      </div>
+      
     )
   }
 }
